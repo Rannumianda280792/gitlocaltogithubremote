94ffd426d8d541de5349ad4720031777bdbf913b
rannumianda280792 <59186375+Rannumianda280792@users.noreply.github.com>
2023-06-11 15:31:52 +0700

    tambahkan file index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BELAJAR GIT GUB</title>
+</head>
+
+<body>
+    <h1>mengirim git local ke github remote</h1>
+    <p>buka folder repo local ,sebelum nya harus di init dulu</p>
+    <p>commit seperti biasa nya</p>
+    <p>di github buat repo sesuai dengan nama folder git local nya</p>
+    <p>di file Read jangan di isi atau di edit, ini penting kerena link copy remote akan di tulis di dalam nya secara auto</p>
+
+
+</body>
+
+</html>
